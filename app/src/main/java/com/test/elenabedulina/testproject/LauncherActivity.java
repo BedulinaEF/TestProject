a1496bafc218f4d30bf321d0c5d1ca45ae941986
elenabedulina <khroll@yandex.ru>
2018-07-05 20:51:55 +0300

    Project created. LauncherActivity added.

---
+++ app/src/main/java/com/test/elenabedulina/testproject/LauncherActivity.java
@@ -0,0 +1,15 @@
+package com.test.elenabedulina.testproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+
+public class LauncherActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher);
+
+    }
+}
