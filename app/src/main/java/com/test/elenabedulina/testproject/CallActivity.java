8b99b6edf7e8a3f50459513b0e222eafde84abae
elenabedulina <khroll@yandex.ru>
2018-07-12 20:32:17 +0300

    Made dialog

--- app/src/main/java/com/test/elenabedulina/testproject/CallActivity.java
+++ app/src/main/java/com/test/elenabedulina/testproject/CallActivity.java
@@ -1,11 +1,15 @@
 package com.test.elenabedulina.testproject;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 
 public class CallActivity extends Activity {
@@ -34,6 +38,7 @@ public class CallActivity extends Activity {
         processResponseCallStart();
         hideProgress();
         makeCall();
+        dialogCallCenter();
 
     }
     private void processResponseCallStart(){
@@ -62,6 +67,46 @@ public class CallActivity extends Activity {
     }
 
     private void processResponseCalling() {
+
+    }
+    private Dialog dialogCallCenter(){
+        final int dialog_exit=1;
+        showDialog(dialog_exit);
+        protected Dialog onCreateDialog(int id){
+            if (id==dialog_exit) {
+                AlertDialog.Builder adb=new AlertDialog.Builder(this);
+                adb.setTitle(R.string.dialog_title);
+                adb.setMessage(R.string.dialog_message);
+                adb.setPositiveButton(R.string.yes_btn, dialogClickListener);
+                adb.setNegativeButton(R.string.no_btn, dialogClickListener);
+                adb.setNeutralButton(R.string.fail_btn, dialogClickListener);
+                adb.setCancelable(false);
+                return adb.create();
+            }
+            return super.onCreateDialog(id);
+        }
+        View.OnClickListener dialogClickListener=new View.OnClickListener(){
+            @Override
+
+            public void onClick(DialogInterface dialog, int btn_choise){
+                switch (btn_choise){
+                    case Dialog.BUTTON_POSITIVE;
+                    saveData();
+                    finish();
+                    break;
+                    case Dialog.BUTTON_NEGATIVE;
+                    finish();
+                    break;
+                    case Dialog.BUTTON_NEUTRAL;
+                    break;
+                }
+            }
+
+        };
+
+        public void saveData(){
+            Toast.makeText(this, R.string.dialog_saved_data, Toast.LENGTH_SHORT).show();
+        }
 
     }
 
