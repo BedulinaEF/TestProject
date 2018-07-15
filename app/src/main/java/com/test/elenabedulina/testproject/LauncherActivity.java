02983c5b9e7dc517411676633545aed1f18f8080
elenabedulina <khroll@yandex.ru>
2018-07-15 17:24:13 +0300

    Made validation

--- app/src/main/java/com/test/elenabedulina/testproject/LauncherActivity.java
+++ app/src/main/java/com/test/elenabedulina/testproject/LauncherActivity.java
@@ -10,30 +10,44 @@ import android.widget.EditText;
 
 
 public class LauncherActivity extends Activity {
+    private EditText etInn;
+    private EditText etPassword;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launcher);
 
-        EditText etInn=findViewById(R.id.et_inn_id);
-
-
-        EditText etPassword=findViewById(R.id.et_password_id);
+        etInn = findViewById(R.id.et_inn_id);
+        etPassword = findViewById(R.id.et_password_id);
 
 
         Button btnLogin = findViewById(R.id.button_login);
         btnLogin.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent launcherIntent=new Intent(LauncherActivity.this,
+                Intent launcherIntent = new Intent(LauncherActivity.this,
                         CallActivity.class);
-                startActivity(launcherIntent);
-
-
-
+                if (checkInput()) {
+                    startActivity(launcherIntent);
+                }
             }
         });
+    }
 
+    private boolean checkInput() {
+        String regexInn = "[0-9]{12}";
+        String regexPassword = "[a-zA-Z0-9]{6,12}";
+        boolean innIsCorrect = etInn.getText().toString().matches(regexInn);
+        boolean passwordIsCorrect = etPassword.getText().toString().matches(regexPassword);
 
-
+        if (!innIsCorrect) {
+            etInn.setError(getString(R.string.wrong_inn));
         }
+
+        if (!passwordIsCorrect) {
+            etPassword.setError(getString(R.string.wrong_password));
+        }
+
+        return innIsCorrect && passwordIsCorrect;
+    }
 }
