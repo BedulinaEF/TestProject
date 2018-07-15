7d26f2c0953f1b3df8417a1868b967e02e2bf910
elenabedulina <khroll@yandex.ru>
2018-07-15 15:56:48 +0300

    Made all dialog

--- app/src/main/java/com/test/elenabedulina/testproject/CallActivity.java
+++ app/src/main/java/com/test/elenabedulina/testproject/CallActivity.java
@@ -4,8 +4,10 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -74,13 +76,40 @@ public class CallActivity extends Activity {
 
 
     }
+    private void doDialogRequest(String  answer){
+        Log.d(CallActivity.class.getSimpleName(), answer);
+
+    }
 
     private void showDialogCallCenter() {
         Dialog dialogCallCenter = new AlertDialog.Builder(this)
-                .setTitle(R.string.dialog_call_center_title)
+                .setTitle(R.string.call_center_message)
+                .setNeutralButton(R.string.fail_btn, new DialogInterface.OnClickListener(){
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        String answer=getString(R.string.fail_btn).toLowerCase();
+                        doDialogRequest(answer);
+
+                    }
+                })
+                .setNegativeButton(R.string.no_btn, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        String answer=getString(R.string.no_btn).toLowerCase();
+                        doDialogRequest(answer);
+                    }
+                })
+                .setPositiveButton(R.string.yes_btn, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        String answer=getString(R.string.yes_btn).toLowerCase();
+                        doDialogRequest(answer);
+                    }
+                })
+
                 .create();
         dialogCallCenter.show();
-        
+
     }
 
 
